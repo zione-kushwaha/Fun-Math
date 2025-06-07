c9999250de655abb2486612077f756cc52f8a3b4
Jeevan kumar kushwaha <jeevankushwaha461@gmail.com>
2025-06-07 11:37:19 +0545

    setup

---
+++ lib/app/app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class FunMathApp extends StatelessWidget {
+  const FunMathApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Fun Math',
+     home: Scaffold(),
+    );
+  }
+}
